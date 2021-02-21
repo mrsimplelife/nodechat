be06a188698b7ac932eb6cd980dd8c973a496f33
youme <mrsimplelife@naver.com>
2021-02-21 15:34:21 +0900

    Add socket.io

--- socket.js
+++ socket.js
@@ -1,21 +1,43 @@
-const WebSocket = require("ws");
+const SocketIO = require("socket.io");
 module.exports = (server) => {
-  const wss = new WebSocket.Server({ server });
-  wss.on("connection", (ws, req) => {
+  const io = SocketIO(server, { path: "/socket.io" });
+  io.on("connection", (socket) => {
+    const req = socket.request;
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("new client", ip);
-    ws.on("message", (message) => {
-      console.log(message);
+    console.log("new client", ip, socket.id, req.ip);
+    socket.on("disconnect", () => {
+      console.log("finish client", ip, socket.id);
+      clearInterval(socket.interval);
     });
-    ws.on("error", (error) => {
-      console.error(error);
+    socket.on("error", (error) => {
+      console.log(error);
     });
-    ws.on("close", () => {
-      console.log("close", ip);
-      clearInterval(ws.interval);
+    socket.on("reply", (error) => {
+      console.log(error);
     });
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) ws.send("hi");
+    socket.interval = setInterval(() => {
+      socket.emit("news", "hi");
     }, 3000);
   });
 };
+// const WebSocket = require("ws");
+// module.exports = (server) => {
+//   const wss = new WebSocket.Server({ server });
+//   wss.on("connection", (ws, req) => {
+//     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+//     console.log("new client", ip);
+//     ws.on("message", (message) => {
+//       console.log(message);
+//     });
+//     ws.on("error", (error) => {
+//       console.error(error);
+//     });
+//     ws.on("close", () => {
+//       console.log("close", ip);
+//       clearInterval(ws.interval);
+//     });
+//     ws.interval = setInterval(() => {
+//       if (ws.readyState === ws.OPEN) ws.send("hi");
+//     }, 3000);
+//   });
+// };
