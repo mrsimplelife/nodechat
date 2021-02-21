5dd3745c485b7b3a51d1bd48d21338f405d1d5a3
youme <mrsimplelife@naver.com>
2021-02-21 15:55:12 +0900

    Add schemas

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
-const dotenv = require("dotenv");
 const WebSocket = require("./socket");
-dotenv.config();
 const indexRouter = require("./routes");
+const connect = require("./schemas");
+connect();
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
---
+++ schemas/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const { MONGO_ID, MONGO_PASSWORD, NODE_ENV } = process.env;
+const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/admin`;
+
+const connect = () => {
+  if (NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+  mongoose.connect(
+    MONGO_URL,
+    {
+      dbName: "nodechat",
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    },
+    (error) => {
+      if (error) {
+        console.log("error", error);
+      } else {
+        console.log("success");
+      }
+    }
+  );
+};
+
+mongoose.connection.on("error", (error) => {
+  console.error("error", error);
+});
+mongoose.connection.on("disconnected", () => {
+  console.error("disconnected try again");
+  connect();
+});
+
+module.exports = connect;
